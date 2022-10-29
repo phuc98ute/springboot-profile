5abf31cab58aad46e89c663c7b41d692d3af7ee3
phuc98ute <vophuc3@gmail.com>
2022-10-30 02:01:56 +0700

    create application profile

---
+++ springboot-application-profile/src/main/java/vn/phucvo/springbootapplicationprofile/SpringbootApplicationProfileApplication.java
@@ -0,0 +1,13 @@
+package vn.phucvo.springbootapplicationprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplicationProfileApplication.class, args);
+    }
+
+}
